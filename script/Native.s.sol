783337fa8b4303e630cfc239b7153330f79dd36b
Francesco <francesco@fulminlabs.org>
2023-12-11 22:05:29 +0100

    add better script
    Signed-off-by: Francesco <francesco@fulminlabs.org>

--- script/Native.s.sol
+++ script/Native.s.sol
@@ -2,21 +2,29 @@
 pragma solidity ^0.8.23;
 
 import {Script, console2 as console} from "forge-std/Script.sol";
+import {BetterScript} from "./BetterScript.s.sol";
 import {UnwrapHook, WETH} from "src/native/UnwrapHook.sol";
 import {NativeTrader, IFloodPlain} from "src/native/NativeTrader.sol";
 
-contract NativeScript is Script {
-    function run() public {
-        vm.broadcast();
-    }
-
+contract NativeScript is BetterScript {
     function deployUnwrapHook(WETH weth, address floodPlain) public {
-        vm.broadcast();
-        new UnwrapHook(weth, floodPlain);
+        bytes32 salt = bytes32(0xe312ba886ebb062d6230c385687badf67809de670f2cc521818f00000000b71e);
+
+        console.logBytes32(initCodeHash(type(UnwrapHook).creationCode, abi.encode(weth, floodPlain)));
+        uint256 key = vm.envUint("ALT_ADMIN_KEY");
+        vm.broadcast(key);
+        console.log(
+            "Deployed UnwrapHook at", deploy2(type(UnwrapHook).creationCode, salt, abi.encode(weth, floodPlain))
+        );
     }
 
     function deployNativeTrader(WETH weth, IFloodPlain floodPlain) public {
-        vm.broadcast();
-        new NativeTrader(weth, floodPlain);
+        bytes32 salt = bytes32(0xe312ba886ebb062d6230c385687badf67809de67261e13702f62000000025ed8);
+        console.logBytes32(initCodeHash(type(NativeTrader).creationCode, abi.encode(weth, floodPlain)));
+        uint256 key = vm.envUint("ALT_ADMIN_KEY");
+        vm.broadcast(key);
+        console.log(
+            "Deployed Native Trader at", deploy2(type(NativeTrader).creationCode, salt, abi.encode(weth, floodPlain))
+        );
     }
 }
